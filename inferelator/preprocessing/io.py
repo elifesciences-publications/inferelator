b6bf930ec7cc59549931df7f920372c792cfa071
asistradition <asistradition@users.noreply.github.com>
2019-09-21 17:08:10 -0400

    More efficient choices

--- inferelator/preprocessing/io.py
+++ inferelator/preprocessing/io.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from inferelator import utils
 
 
 def read_10x_hdf5(file_name, sparse=False, **kwargs):
@@ -11,14 +12,20 @@ def read_10x_hdf5(file_name, sparse=False, **kwargs):
     :return data_frame: pd.DataFrame [N x G]
     """
 
-    import scanpy
+    # Import scanpy
+    try:
+        import scanpy
+    except ImportError as err:
+        raise ImportError("Install scanpy to load hdf5 files") from err
+
     ann_data = scanpy.read_10x_h5(file_name)
+    utils.Debug.vprint("{f} loaded into {sh} annotated data frame".format(f=file_name, sh=ann_data.X.shape))
+
+    data_frame = pd.DataFrame.sparse.from_spmatrix(ann_data.X, index=ann_data.obs_names, columns=ann_data.var_names)
+
     if sparse:
-        data_frame = pd.DataFrame.sparse.from_spmatrix(ann_data.X,
-                                                       index=ann_data.obs_names,
-                                                       columns=ann_data.var_names)
+        utils.Debug.vprint("Data processed into {sh} sparse DataFrame".format(sh=data_frame.shape))
+        return data_frame
     else:
-        data_frame = pd.DataFrame(ann_data.X.todense(),
-                                  index=ann_data.obs_names,
-                                  columns=ann_data.var_names)
-    return data_frame
+        utils.Debug.vprint("Data processed into {sh} dense DataFrame".format(sh=data_frame.shape))
+        return data_frame.sparse.to_dense()
